a46f39eeaa04046ee4e4afeacdb26f8d866c522a
Pablo Sao <pablo_sao@outlook.com>
2019-04-15 15:40:42 -0600

    Update RabbitChase.s
    Se despliega matriz armada en prueba, se pasara a subrutina para impresión

--- RabbitChase.s
+++ RabbitChase.s
@@ -33,11 +33,14 @@ ingreso:
 
 	LDR   R1, [R1]			@ Cargamos valor ingresado
 
+	MOV   R4, #-4			@ Seteamos dirección inicial de matriz
+	MOV   R5, #4			@ seteamos contador de impresión
+
 	CMP   R1, #0x31			@ Si R1 == 1
-	BEQ   _play			@   Inicia juego
+	BLEQ   _play			@   Inicia juego
 
 	CMP   R1, #0x32			@ Si R2 == 2
-	BEQ   _exit			@   Sale del juego
+	BLEQ   _exit			@   Sale del juego
 
 	BL    main
 
@@ -45,9 +48,22 @@ _play:
 
 	/* Armamos matriz inicial */
 
+	LDR   R0,=display		@ Cargamos dirección de despliegue
+	LDR   R1,=tablero		@ Dirección de matriz
+
+	ADD   R4, #4			@ indice de matriz
+	ADD   R1, R4			@ nos posicionamos en el valor del indice
+	BL    printf			@ se imprime el valor
+
+	SUBS  R5, #1			@ restamos contador de ciclo
+	CMP   R5, #0			@ Si R5 no == 1
+	BNE   _play			@    retornamos a _play
+
+	B     _exit			@ de lo contrario salimos
 
 _exit:
 	LDMFD SP!,{LR}
+
 	BX    LR
 
 .data
@@ -55,6 +71,13 @@ _exit:
 
 mensaje_ingreso:
 	.asciz "Ingrese Opción: "
+
+display:
+	.asciz "%s | "
+
+tablero:
+	.asciz "(1)","(2)"
+	.asciz "(3)","(4)"
 
 vingreso:
 	.asciz ""
