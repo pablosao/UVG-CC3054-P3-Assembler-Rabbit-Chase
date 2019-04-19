0814f64af93d8385cc9d9af9e94d377d180c9108
Pablo Sao <pablo_sao@outlook.com>
2019-04-19 10:41:20 -0600

    se agregan variables globales para posición de jugador y conejo y control de despliegue

--- var.s
+++ var.s
@@ -1,4 +1,67 @@
 
+/************************************************************************************/ 
+/*         Autor: Pablo Sao                                                         */ 
+/*         Fecha: 19 de abril de 2019                                               */ 
+/*   Descripcion: variables de acceso global                                        */ 
+/*				                                                    */ 
+/************************************************************************************/ 
+
+/*     Despliegue de jugadores    */
+.text
+.align 2
+.global displayUsr
+displayUsr:
+	.asciz "(H) "
+
+.text
+.align 2
+.global displayConejo
+displayConejo:
+	.asciz "(H) "
+
+.text
+.align 2
+.global clsDisplay
+clsDisplay:
+	.asciz "( ) "
+
+.text
+.align 2
+.global displayConejo
+displayCapConejo:		@ Captura de Conejo
+	.asciz "(x) "
+
+
+/*     Posición jugador    */
+.text
+.align 2
+.global colUsr
+colUsr:
+	.word 12
+
+.text
+.align 2
+.global filaUsr
+filaUsr:
+	.word 1
+
+
+
+/*     Posición conejo    */
+.text
+.align 2
+.global colConejo
+colConejo:
+	.word 12
+
+.text
+.align 2
+.global filaConejo
+filaConejo:
+	.word 3
+
+
+/*   Tablero de 5x5     */
 .text
 .align 2
 .global fila1
